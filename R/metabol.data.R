9a2e35e965c10b04a6ce9d7be478677f287ca520
Shubbham Gupta <shubbham28@gmail.com>
2022-06-14 10:33:18 +0100

    Update metabol.data.R

--- R/metabol.data.R
+++ R/metabol.data.R
@@ -1,25 +1,25 @@
-#'  Metabolites data of individuals
+#'  Metabolite data of individuals
 #'
-#' A random dataset that contains metabolites levels of 3 metabolites for different individuals taken on different timepoints.
+#' A simulated dataset containing metabolite levels of 3 metabolites for different individuals taken on four different time points.
 #'
-#' @format A data frame with 164 rows and 16 columns. NA in the data means that the metabolite value is missing for the individual for that timepoint.
+#' @format A data frame with 164 rows and 16 columns. Missing values for the individual for that time point is showed as NA.
 #' \describe{
-#'   \item{Age}{This contains the age of the individual}
-#'   \item{BMI}{This contains the BMI value of the individual}
-#'   \item{SexM.1F.2}{Gender of the individual is recorded here. It's a factor column where 1 denotes "Male" while 2 denotes "Female"}
-#'   \item{metabolA_1}{Contains the value of "A" metabolite for the first timepoint.}
-#'   \item{metabolA_2}{Contains the value of "A" metabolite for the second timepoint.}
-#'   \item{metabolA_3}{Contains the value of "A" metabolite for the third timepoint.}
-#'   \item{metabolA_4}{Contains the value of "A" metabolite for the fourth timepoint.}
-#'   \item{metabolB_1}{Contains the value of "B" metabolite for the first timepoint.}
-#'   \item{metabolB_2}{Contains the value of "B" metabolite for the second timepoint.}
-#'   \item{metabolB_3}{Contains the value of "B" metabolite for the third timepoint.}
-#'   \item{metabolB_4}{Contains the value of "B" metabolite for the fourth timepoint.}
-#'   \item{metabolC_1}{Contains the value of "C" metabolite for the first timepoint.}
-#'   \item{metabolC_2}{Contains the value of "C" metabolite for the second timepoint.}
-#'   \item{metabolC_3}{Contains the value of "C" metabolite for the third timepoint.}
-#'   \item{metabolC_4}{Contains the value of "C" metabolite for the fourth timepoint.}
-#'   \item{Individual_id}{Contains the individual id for the individual that provided the measurements for various metabolites.}
+#'   \item{Age}{The age of the individuals.}
+#'   \item{BMI}{The BMI of the individuals.}
+#'   \item{SexM.1F.2}{The gender of the individuals. It is a factor where 1 denotes "Male" while 2 denotes "Female".}
+#'   \item{metabolA_1}{The value of metabolite "A" for the first timepoint.}
+#'   \item{metabolA_2}{The value of metabolite "A" for the second timepoint.}
+#'   \item{metabolA_3}{The value of metabolite "A" for the third timepoint.}
+#'   \item{metabolA_4}{The value of metabolite "A" for the fourth timepoint.}
+#'   \item{metabolB_1}{The value of metabolite "B" for the first timepoint.}
+#'   \item{metabolB_2}{The value of metabolite "B" for the second timepoint.}
+#'   \item{metabolB_3}{The value of metabolite "B" for the third timepoint.}
+#'   \item{metabolB_4}{The value of metabolite "B" for the fourth timepoint.}
+#'   \item{metabolC_1}{The value of metabolite "C" for the first timepoint.}
+#'   \item{metabolC_2}{The value of metabolite "C" for the second timepoint.}
+#'   \item{metabolC_3}{The value of metabolite "C" for the third timepoint.}
+#'   \item{metabolC_4}{The value of metabolite "C" for the fourth timepoint.}
+#'   \item{Individual_id}{The id of the individuals.}
 #' }
 #' @usage data(metabol.data)
 "metabol.data"
