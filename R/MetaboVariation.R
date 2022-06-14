feebea3c99824af930a53366fbb7cb66aec71e4b
Shubbham Gupta <shubbham28@gmail.com>
2022-06-14 10:32:05 +0100

    Update MetaboVariation.R

--- R/MetaboVariation.R
+++ R/MetaboVariation.R
@@ -1,42 +1,53 @@
 `%dopar%` <- foreach::`%dopar%`
 #' @title
-#' Models the metabolomic profiles to get individual traits
-#'@description
-#' Fits a generalised linear model on the passed metabolites and provide results for single or multiple metabolites. It works on bayesian framework using the function \code{\link[brms]{brm}} from BRMS package.
+#' Fits a Bayesian generalised linear model to repeated measurements of metabolites.
+#' @description
+#' Implements a Bayesian generalised linear model (BGLM) to identify the flagged individuals with notable variations.
+#' @details
+#' TheBayesian generalised linear model (BGLM) to identify individuals with variations in their
+#' metabolite levels is fitted by employing STAN. The BGLM includes covariates  of the individuals and models the repeated measurements to explore intra-individual variation. Individuals with notable variation in metabolite levels are flagged.
+#' The model builds on the Bayesian framework using the function \code{\link[brms:brm]{brm}} from \code{\link[brms:brm]{brm}} package.
 #'
-#' @param data An object of class data.frame (or one that can be coerced to that class) containing data of all variables used for modelling.
-#' @param individual_ids A character string that denotes the column name of individual ids present in the dataset passed.
-#' @param metabolite A string or vector containing metabolites name that needs to be modeled.
-#' @param covariates A list of column names present in the data that denotes the covariates data for the individuals in the data.
-#' @param save_brms_model A logical value indicating whether the model should be saved or not. By default, it is set as FALSE
-#' @param full_posterior A logical value indicating whether the full posterior distribution should be included or not. By default, it is set as FALSE and the summarized result is passed by the function.
+#' The BGLM models the metabolite value \eqn{M_{ij}} for the individual \eqn{i} at timepoint \eqn{j},
+#' \deqn{ M_{ij} = \beta_{0} + \beta_{1}X_i + r_i + \epsilon_{ij}}
+#'
+#' where \eqn{i \in 1, 2, ..., n} denotes individual \eqn{i} of the \eqn{n} individuals in the cohort and \eqn{j \in 1, 2, ..., k} denotes the timepoints at which metabolites are measured. The intercept of the model is denoted by \eqn{\beta_0}, model coefficients for \eqn{m} covariates are denoted by \eqn{\beta_{1}} which is in form of \eqn{1 * m} matrix and \eqn{m} covariates of the \eqn{i^{th}} individual is represented by \eqn{X_i} in theform of the diagonal matrix of \eqn{m * m}.
+#' The random effect due to repeated measurements of the \eqn{i^{th}} individual is represented by \eqn{r_i} and \eqn{\epsilon_{ij}} indicates the model's error term for individual \eqn{i} at time point \eqn{j}.
+#'
+#' @param data A data frame containing data of all variables to be used in the BGLM.
+#' @param individual_ids A character string detailing the name of the column in the data that contains the individual ids.
+#' @param metabolite A string or vector of string containing the name of metabolites to model.
+#' @param covariates A list of string containing the name of columns in the data that contains the covariates.
+#' @param save_brms_model A logical value indicating whether the model should be saved or not. By default, it is set as FALSE.
+#' @param full_posterior A logical value indicating whether the full posterior distribution should be returned or not. By default, it is set as FALSE. #'
 #' @param iter Number of total iterations per chain (including warmup; defaults to 2000).
 #' @param warmup A positive integer specifying number of warmup (aka burnin)
 #'   iterations. This also specifies the number of iterations used for stepsize
 #'   adaptation, so warmup draws should not be used for inference. The number
 #'   of warmup should not be larger than \code{iter} and the default is
 #'   \code{iter/2}.
+#' @param thin Thinning rate. Must be a positive integer. Set thin > 1 to save memory and computation time if iter is large.
+#' @param cutoff Cutoff for the credible interval of posterior predictive distribution. The value should be less than 100. Default is set to 95.
 #' @param ... Further arguments are passed to \code{\link[brms]{brm}} for backend processes.
 #'
-#' @return modelling returns an object of "MetaboVariation" class with "meta.single_model" and "meta.multi_model" subclass. Sublass "meta.multi_model" is a collection of subclass "meta.single_model" objects.
-#' And subclass "meta.single_model" contains following values.
+#' @return function returns an object of "MetaboVariation". Following values are returned for each metabolite object.
 #' \itemize{
-#' \item significant_covariates - a list of covariates that influences the metabolite value significantly for the individuals. Based on the posterior distribution of the covariates, if zero lies in the 90% credible interval, it is assumed that particular covariate has no significant realtion with the metabolite value.
-#' \item result - It stores a summarized result for the posterior predictive distribution of the cohort. It contains the mean, 95% credible interval range, tails of the credible interval and the original value of the individual for that timepoint. It also has a flag that shows whether the original values lies within the credible interval or not.
-#' \item iterations - Number of total iterations per chain used for modelling (including warmup; defaults to 2000).
-#' \item warmup - A positive integer specifying number of warmup (aka burnin) iterations. This also specifies the number of iterations used for stepsize adaptation, so warmup draws should not be used for inference. The number of warmup should not be larger than iter and the default is iter/2.
+#' \item significant_covariates - a list of covariates that influences the metabolite value significantly for the individuals.
+#' \item result - A summarized result for the posterior predictive distribution of the cohort. It contains the mean, \code{cutoff}% credible interval range, tails of the credible interval and the original value of the individual for that timepoint. It also has a flag value that shows whether the original value lies within the credible interval or not.
+#' \item iter - Number of total iterations per chain used for modelling.
+#' \item warmup - Number of total warmup iterations per chain.
 #' \item Rhat - Rhat refers to the potential scale reduction statistic, also known as the Gelman-Rubin statistic.
 #' Thus, it measures the extent to which chains are converging. The further the value of the statistic from 1, the worse. Model is considered good if the value lies between 0.9 and 1.05.
 #' \item metabolite - Contains the name of metabolite on which modelling is done.
-#' \item full_posterior - It contains the full posterior predictive distiribution of the data. It can be used for further analysis as per the user requirements.
+#' \item full_posterior - It contains the full posterior predictive distribution of the data. It can be used for further analysis as per the user requirements.
 #' }
 #' @export
-#'
+#' @seealso \code{\link{flaggedIndividuals}}, \code{\link{plot.MetaboVariation}}
 #' @examples
 #' \dontrun{
 #' data(metabol.data)
 #' metabolite_list = colnames(metabol.data)[5:length(colnames(metabol.data))]
-#' metabolites = get.metabolites(list = metabolite_list)
+#' metabolites = getmetabolites(list = metabolite_list)
 #' covariates = c("SexM.1F.2","Age","BMI")
 #' individual_id = "Sample.Id"
 #' model = MetaboVariation(data = metabol.data,individual_ids = individual_id,
@@ -48,7 +59,7 @@
 #' @import parallel foreach stats
 
 MetaboVariation <- function ( data,individual_ids,metabolite,covariates=NULL, save_brms_model = FALSE,full_posterior = FALSE,iter = 2000,
-          warmup = floor(iter/2),...){
+          warmup = floor(iter/2),thin = 1,cutoff=95,...){
 
   future = getOption("future", TRUE)
   file = paste(metabolite,"model",sep="_")
@@ -81,6 +92,8 @@ MetaboVariation <- function ( data,individual_ids,metabolite,covariates=NULL, sa
   else{
     formula = paste("measurement ~",paste(covariates,collapse = " + "), "+ (1|",individual_ids,")")
   }
+  lower_cutoff = round((100 - cutoff)/2,1)
+  upper_cutoff = round(100 - lower_cutoff,1)
   if(length(metabolite)==1){
     var_list = na.omit(stringr::str_extract(col_list,stringr::regex(paste(".*",metabolite,".*",sep = ""))))
     if(length(var_list)==0){
@@ -90,7 +103,7 @@ MetaboVariation <- function ( data,individual_ids,metabolite,covariates=NULL, sa
     new_data = na.omit(new_data)
     print(paste("Model building for",metabolite))
     model = brms::brm(formula,data = new_data,iter = iter,
-                      warmup = warmup, future = future, seed = seed, file = file)
+                      warmup = warmup, future = future, seed = seed, file = file,thin = thin)
     rows <- nrow(new_data)
     doParallel::registerDoParallel(cores)
     print(paste("Calculating posterior predictive distribution for",metabolite))
@@ -104,10 +117,10 @@ MetaboVariation <- function ( data,individual_ids,metabolite,covariates=NULL, sa
     colnames(result) = result[1,]
     result = result[-1,]
     result = apply(result,2,as.numeric)
-    brief_result = t(apply(result, 2, function(x) {c(mean(x),quantile(x,probs = c(0.025,0.975)),quantile(x,0.975)-quantile(x,0.025))}))
+    brief_result = t(apply(result, 2, function(x) {c(mean(x),quantile(x,probs = c(lower_cutoff/100,upper_cutoff/100)),quantile(x,upper_cutoff/100)-quantile(x,lower_cutoff/100))}))
     brief_result = cbind(brief_result,new_data$measurement)
     brief_result = cbind(brief_result,(brief_result[,2] < brief_result[,5] & brief_result[,5] < brief_result[,3]))
-    colnames(brief_result) = c("mean","2.5%","97.5%","QR","original","identifier")
+    colnames(brief_result) = c("mean",paste0(lower_cutoff,"%"),paste0(upper_cutoff,"%"),"QR","original","identifier")
     co_list = names(na.omit(apply(summary(model)$fixed[2:nrow(summary(model)$fixed),3:4],1,function(x) {ifelse(prod(x[1],x[2])>=0,1,NA )})))
     if(identical(co_list,character(0))){
       co_list = NULL
@@ -135,7 +148,7 @@ MetaboVariation <- function ( data,individual_ids,metabolite,covariates=NULL, sa
         new_data = na.omit(new_data)
         print(paste("Model building for",met))
         model = brms::brm(formula,data = new_data,iter = iter,
-                          warmup = warmup, future = future, seed = seed, file = file)
+                          warmup = warmup, future = future, seed = seed, file = file,thin = thin)
 
         rows <- nrow(new_data)
         doParallel::registerDoParallel(cores)
@@ -150,10 +163,10 @@ MetaboVariation <- function ( data,individual_ids,metabolite,covariates=NULL, sa
         colnames(result) = result[1,]
         result = result[-1,]
         result = apply(result,2,as.numeric)
-        brief_result = t(apply(result, 2, function(x) {c(mean(x),quantile(x,probs = c(0.025,0.975)),quantile(x,0.975)-quantile(x,0.025))}))
+        brief_result = t(apply(result, 2, function(x) {c(mean(x),quantile(x,probs = c(lower_cutoff/100,upper_cutoff/100)),quantile(x,lower_cutoff/100)-quantile(x,upper_cutoff/100))}))
         brief_result = cbind(brief_result,new_data$measurement)
         brief_result = cbind(brief_result,(brief_result[,2] < brief_result[,5] & brief_result[,5] < brief_result[,3]))
-        colnames(brief_result) = c("mean","2.5%","97.5%","QR","original","identifier")
+        colnames(brief_result) = c("mean",paste0(lower_cutoff,"%"),paste0(upper_cutoff,"%"),"QR","original","identifier")
         co_list = names(na.omit(apply(summary(model)$fixed[2:nrow(summary(model)$fixed),3:4],1,function(x) {ifelse(prod(x[1],x[2])>=0,1,NA )})))
         if(identical(co_list,character(0))){
           co_list = NULL
