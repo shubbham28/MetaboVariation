9080217769e68acf8a1a1149cf2edf8905fbfaef
Shubbham Gupta <shubbham28@gmail.com>
2022-06-14 10:33:37 +0100

    Update plot.R

--- R/plot.R
+++ R/plot.R
@@ -3,27 +3,24 @@ globalVariables(c("timepoint","value"))
 unit = grid::unit
 
 #' @title
-#' Plot results from "MetaboVariation" object.
+#' Plots the flagged individuals for the metabolites.
 #' @description
-#' Plot the results for the class "MetaboVariation". It plots using plotly and circlize package.
+#' A function to plot the results after fitting a Bayesian generalised linear model (BGLM) model to metabolomic data.
+#' A circos plot or a barplot can be produced depicting results from different perspectives.
 #'
-#' @param x An object of class "MetaboVariation"
-#' @param type A character specifying the plot type (eg: "circos", "individual_count", "metabolites_count"). By default, "circos" is shown.
+#' @param x An object of class \code{\link{MetaboVariation}}
+#' @param type A character specifying the plot type (eg: "circos", "metabolites_count"). By default, "circos" is shown.
 #' @param timepoints A numeric vector containing timepoints to be shown in the plots. By default, it is NULL and will show all the timepoints
-#' @param threshold A number to filter out individuals that have been flagged in less than "threshold" metabolites. By default, it is set at 2.
 #' @param ... Further arguments are ignored.
 #'
-#' @return plot will show the results of modelling in the following three ways
+#' @return plot will show the results of modelling in the following two ways:
 #' \itemize{
-#'   \item Circos plot - A circular plot that will show the 95% credible interval for all the individuals across timepoints. If the actual value
-#'   is outside the interval, that bar will be flagged as black and the individual's id will be flagged and shown in \bold{bold}.
-#'   \item Metabolites count plot - A bar plot that shows the number of individuals are flagged in each timepoints out of all individuals.
-#'   For "meta.single_model" subclass, number of bars will be same as the number of timepoints for that single metabolite. For meta.multi_model" subclass, each timepoints is shown with
-#'   different color for each metabolite present in the subclass.
-#'   \item Individual count plot - A heatmap that shows how many time a particular individual is flagged for a specific metabolite. The minimum count
-#'   is 0 while the maximum count is the number of total timepoints passed. This plot is only valid for "meta.multi_model" subclass.
+#'   \item Circos plot - A circular plot that will show the credible interval for all the individuals across timepoints. If the observed value
+#'   is outside the credible interval, that bar will be flagged as black and the individual's id will be shown in __bold__.
+#'   \item Metabolites count plot - A bar plot that shows the number of individuals that are flagged in each timepoint for all metabolites passed.
 #' }
-#'
+#' @seealso \code{\link{MetaboVariation}}
+
 #'
 #' @examples
 #' \dontrun{
@@ -36,13 +33,13 @@ unit = grid::unit
 #' metabolite = metabolites[1:3], covariates = covariates,full_posterior = TRUE)
 #' plot(x = model, type = "circos")
 #' plot(x = model, type = "metabolites_count",timepoints = c(1,2,4))
-#' plot(x = model, type = "individual_count",threshold = 3)
+#' plot(x = model, type = "individual_count")
 #'}
 #'@aliases plot
 #' @method plot MetaboVariation
 #' @export
 #'
-plot.MetaboVariation <- function(x,type = "circos",timepoints = NULL,threshold = 2,...){
+plot.MetaboVariation <- function(x,type = "circos",timepoints = NULL,...){
   model = x
   if(!inherits(model,"MetaboVariation")){
     stop("Model passed is not of class 'Metabovariation'")
@@ -55,9 +52,6 @@ plot.MetaboVariation <- function(x,type = "circos",timepoints = NULL,threshold =
    #Single metabolite model
   if(inherits(model,"meta.single_model")){
 
-    if(type == "individual_count"){
-      stop("Single model does not support this kind of plot")
-      }
     #Circos plot
     if(type == "circos"){
     values = cbind.data.frame(stringr::str_split_fixed(rownames(model$result),pattern=" ",n=2),model$result[,4])
@@ -100,9 +94,9 @@ plot.MetaboVariation <- function(x,type = "circos",timepoints = NULL,threshold =
     else if(is.numeric(timepoints) & max(timepoints) <= ncol(values)-1){
 
       height = 0.6/length(timepoints)
-      for (i in rev(timepoints)) {
+      for (i in timepoints) {
         i = i+1
-        status[,i] = sapply(status[,i], function(x){ifelse(x==0,1,colors[i-1])})
+        status[,i] = sapply(status[,i], function(x){ifelse(x==0,1,colors[i])})
         circlize::circos.track(ylim = c(0,1),bg.border=NA,track.margin=c(0,0),track.height=height, panel.fun = function(x, y) {
           value = scales::rescale(values[,i])
           circlize::circos.barplot(value, 1:nrow(values),border = "white",bar_width = 0.75, col = status[,i])
@@ -139,8 +133,8 @@ plot.MetaboVariation <- function(x,type = "circos",timepoints = NULL,threshold =
         colnames(sum_plot) = c("sum_m","Timepoint")
       }
       sum_plot$Timepoint = rownames(sum_plot)
-      plotly::plot_ly(sum_plot,x = ~Timepoint, y = ~sum_m, type = "bar") %>%
-        plotly::layout(yaxis = list(title = 'Individual Count'), barmode = 'group')
+      plotly::plot_ly(sum_plot,x = ~Timepoint, y = ~sum_m, type = "bar",marker = list(color=colors[1],width = 0.2)) %>%
+        plotly::layout(yaxis = list(title = 'No. of individuals'))
     }    # End Metabolite count plot
 
   }
@@ -192,9 +186,9 @@ plot.MetaboVariation <- function(x,type = "circos",timepoints = NULL,threshold =
       else if(is.numeric(timepoints) & max(timepoints) <= ncol(values)-1){
 
         height = 0.6/length(timepoints)
-        for (i in rev(timepoints)) {
+        for (i in timepoints) {
           i = i+1
-          status[,i] = sapply(status[,i], function(x){ifelse(x==0,1,colors[i-1])})
+          status[,i] = sapply(status[,i], function(x){ifelse(x==0,1,colors[i])})
           circlize::circos.track(ylim = c(0,1),bg.border=NA,track.margin=c(0,0),track.height=height, panel.fun = function(x, y) {
             value = scales::rescale(values[,i])
             circlize::circos.barplot(value, 1:nrow(values),border = "white",bar_width = 0.75, col = status[,i])
@@ -244,51 +238,13 @@ plot.MetaboVariation <- function(x,type = "circos",timepoints = NULL,threshold =
      # figure = ggplot2::ggplot(sum_plot, ggplot2::aes(fill=Timepoints, y=count, x=Metabolites)) +  ggplot2::geom_bar(position="dodge", stat="identity")
       #print(figure)
       #plotly::ggplotly(figure)
-     figure = plotly::plot_ly(x = sum_plot[["Metabolites"]],y = sum_plot[[paste0("Timepoint.",timepoints[1])]], type = "bar",marker = list(color=colors[timepoints[1]]),name = paste0("Timepoint ",timepoints[1])) %>%
-      plotly::layout(yaxis = list(title = 'individual Count',range = c(min(sum_plot[,-1]) - 1,max(sum_plot[,-1]) + 1)), barmode = 'group')
+     figure = plotly::plot_ly(x = sum_plot[["Metabolites"]],y = sum_plot[[paste0("Timepoint.",timepoints[1])]], type = "bar",marker = list(color=colors[timepoints[1]]),width = 0.2,name = paste0("Timepoint ",timepoints[1])) %>%
+      plotly::layout(yaxis = list(title = 'No. of individuals',range = c(min(sum_plot[,-1]) - 1,max(sum_plot[,-1]) + 1)), barmode = 'group')
       for (i in 2:length(timepoints)) {
-      figure = figure %>% plotly::add_trace(y = sum_plot[[paste0("Timepoint.",timepoints[i])]],marker = list(color=colors[i]),name = paste0("Timepoint ",timepoints[i]))
+      figure = figure %>% plotly::add_trace(y = sum_plot[[paste0("Timepoint.",timepoints[i])]],marker = list(color=colors[i]),width = 0.2,name = paste0("Timepoint ",timepoints[i]))
       }
       return(plotly::config(plotly::as_widget(figure)))
     }    # End Metabolite count plot
-
-    #Individual count plot
-    if(type == "individual_count"){
-      if(!is.numeric(threshold)){
-        stop("Threshold value must be an integer")
-      }
-      individuals = c()
-      for (met in names(model)) {
-        status = cbind.data.frame(stringr::str_split_fixed(rownames(model[[met]]$result),pattern=" ",n=2),model[[met]]$result[,6])
-        colnames(status) = c("id","timepoint","status")
-        time_data = as.numeric(gsub("[^0-9]", "",unique(status$timepoint)))
-        status = tidyr::spread(status,timepoint,status)
-        individuals = cbind(individuals,apply(!status[,-1],2,function(x){ifelse(x==FALSE | is.na(x),0,1)}))
-        if(!is.null(timepoints)){
-          if(!all(timepoints %in% time_data)){
-            stop("Timepoints passed are not present in the data")
-          }
-        }
-      }
-      if(is.null(timepoints)){
-        timepoints = ncol(individuals)%/%length(names(model))
-        timepoints = 1:timepoints
-      }
-      plotindividuals = c()
-      for (met in names(model)) {
-        plotindividuals = cbind(plotindividuals,apply(individuals[,paste(model[[met]]$metabolite,"_",timepoints,sep = "")],1,sum))
-      }
-      colnames(plotindividuals) = names(model)
-      rownames(plotindividuals) = status$id
-      summ = apply(individuals, 1,sum)
-      plotindividuals = plotindividuals[summ >= threshold,]
-      print(as.factor(plotindividuals))
-      height = nrow(plotindividuals)*75
-      width = length(names(model))*100
-      plotly::plot_ly(x = names(model), y = paste0("S_",rownames(plotindividuals)),colors = "Blues",
-              z = plotindividuals, type = "heatmap",height = height,width=width) %>%
-        plotly::layout(xaxis = list(title = 'Metabolites'),yaxis = list(title = 'Individual id'))
-    }#End Individual count plot
 
   }
 
