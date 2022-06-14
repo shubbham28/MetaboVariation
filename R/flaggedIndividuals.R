fa22a88a5d05305796afe7d5cce7ba9380676897
Shubbham Gupta <shubbham28@gmail.com>
2022-06-14 10:32:28 +0100

    Update flaggedIndividuals.R

--- R/flaggedIndividuals.R
+++ R/flaggedIndividuals.R
@@ -1,32 +1,34 @@
 
 #' @title
-#' Gives flagged individuals in a tablular format
+#' Lists flagged individuals in tabular format.
 #' @description
-#' From the class "MetaboVariation", the results contain the individuals that have been flagged as they have values outside the 95% central credible interval based on the results.
-#' This function provides the details of those individuals in a tabular format.
+#' A function to list the flagged individuals resulting from fitting a Bayesian generalised linear model (BGLM) model to repeated measures metabolomic data.
 #'
-#' @param model An object of class "MetaboVariation"
-#' @param data An object of class data.frame (or one that can be coerced to that class) containing data of all variables used for modelling.
-#' @param individual_id A character string that denotes the column name of individual ids present in the dataset passed.
-#' @param covariates A list of column names present in the data that denotes the covariates data for the individuals in the data.
-#' @param threshold A number to filter out individuals that have been flagged in less than "threshold" metabolites. By default, it is set at 2.
+#' @param model An object of class \code{\link{MetaboVariation}}.
+#' @param data A data frame containing data of all variables used in the BGLM along with a column detailing the individual ids.
+#' @param individual_id A character string detailing the name of the column in the data frame that contains the individual ids.
+#' @param threshold A positive integer Individuals that have been flagged in less than _threshold_ metabolites will not be included. By default, it is set to 2.
 #'
-#' @return A data.frame of the flagged individuals that have values outside of 95% credible interval.
+#' @return The function will provide two types of lists based on the type of model object.
+#' \itemize{
+#'   \item Single metabolite model - If the model contains result for single metabolite, the function will return a list of flagged individuals along with their metabolite levels across different time points.
+#'   \item Multiple metabolites model - If the model contain results for multiple metabolites, the function will return a list of individuals flagged in those metabolites more than _threshold_ times.
+#' }
+#' @seealso \code{\link{MetaboVariation}}
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' data(metabol.data)
 #' metabolite_list = colnames(metabol.data)[5:length(colnames(metabol.data))]
-#' metabolites = get.metabolites(list = metabolite_list)
-#' covariates = c("SexM.1F.2","Age","BMI")
+#' metabolites = getmetabolites(list = metabolite_list)
 #' individual_id = "Sample.Id"
 #' model = MetaboVariation(data = metabol.data,individual_ids = individual_id
-#' ,metabolite = metabolites[1:3],covariates = covariates,full_posterior = TRUE)
+#' ,metabolite = metabolites,full_posterior = TRUE)
 #' flaggedIndividuals(model,data = metabol.data,
-#' individual_id = individual_id,covariates=covariates)
+#' individual_id = individual_id)
 #' }
-flaggedIndividuals <- function(model,data,individual_id,covariates=NULL,threshold = 2){
+flaggedIndividuals <- function(model,data,individual_id,threshold = 2){
   if(!inherits(model,"MetaboVariation")){
     stop("Model passed is not of class 'Metabovariation'")
   }
@@ -34,13 +36,12 @@ flaggedIndividuals <- function(model,data,individual_id,covariates=NULL,threshol
   if(inherits(model,"meta.single_model")){
     flagged = cbind.data.frame(stringr::str_split_fixed(rownames(model$result),pattern=" ",n=2),"identifier" = model$result[,6])
     flagged = as.factor(unique(flagged[flagged$identifier ==0,1]))
-    flagged = data[data[[individual_id]] %in% flagged,c(individual_id,covariates,na.omit(stringr::str_extract(colnames(data),stringr::regex(paste(".*",model$metabolite,".*",sep = "")))))]
+    flagged = data[data[[individual_id]] %in% flagged,c(individual_id,na.omit(stringr::str_extract(colnames(data),stringr::regex(paste(".*",model$metabolite,".*",sep = "")))))]
     return(as.data.frame(flagged))
   }
 
   # Multiple metabolite model
   else if(inherits(model,"meta.multi_model")) {
-
     individuals = c()
     for (met in names(model)) {
       status = cbind.data.frame(stringr::str_split_fixed(rownames(model[[met]]$result),pattern=" ",n=2),model[[met]]$result[,6])
@@ -58,9 +59,15 @@ flaggedIndividuals <- function(model,data,individual_id,covariates=NULL,threshol
     rownames(plotindividuals) = status$id
     summ = apply(individuals, 1,sum)
     plotindividuals = plotindividuals[summ >= threshold,]
-    individuals = data[data[[individual_id]] %in% as.factor(rownames(plotindividuals)),c(individual_id,covariates)]
-    return(as.data.frame(individuals))
+    output = cbind(rownames(plotindividuals),data.frame(plotindividuals, row.names=NULL))
+    colnames(output) = c("Individual_id",names(model))
+    return(as.data.frame(output))
   }
 
 
 }
+
+
+#Throw
+
+
